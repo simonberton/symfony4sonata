88e2d5092e5bf3f4338bb236681e1d5f95204b9c
Simon Berton <simonberton@gmail.com>
2019-01-07 12:37:24 -0300

    first commit

---
+++ src/Entity/Product.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string The name of this product.
+     *
+     * @ORM\Column
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
