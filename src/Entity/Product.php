5dc793097c199755a6cbc4d6b9a6749603ef970e
Simon Berton <simonberton@gmail.com>
2019-01-08 09:38:16 -0300

    readme and product example

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -27,4 +27,14 @@ class Product
     {
         return $this->id;
     }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
 }
